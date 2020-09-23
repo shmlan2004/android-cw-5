27da6e60c1ac7ba0fb57a4d8b5f8fa74785bdea3
shamlan <shbabttat1@gmail.com>
2020-09-23 21:13:54 +0300

    done_v2

--- cw5/app/src/main/java/com/example/cw5/MainActivity2.java
+++ cw5/app/src/main/java/com/example/cw5/MainActivity2.java
@@ -2,7 +2,10 @@ package com.example.cw5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,6 +16,7 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        Button z = findViewById(R.id.back);
         final TextView n = findViewById(R.id.textView);
         final TextView r = findViewById(R.id.textView2);
 
@@ -23,5 +27,13 @@ public class MainActivity2 extends AppCompatActivity {
         n.setText(sham);
         r.setText(sham1);
 
+        z.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent a = new Intent(MainActivity2.this , MainActivity.class);
+
+                startActivity(a);
+            }
+        });
     }
 }
